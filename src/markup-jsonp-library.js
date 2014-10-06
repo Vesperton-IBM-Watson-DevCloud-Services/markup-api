08335c84766eb73c9fd65b9f6ffb668098dfab7a
M. Maxwell Watson <hi@mmwtsn.com>
2014-10-06 16:59:04 -0400

    namespaces functions, updates html targets

--- src/markup-jsonp-library.js
+++ src/markup-jsonp-library.js
@@ -1,11 +1,20 @@
-function insertStyles(styles) {
-  var open  = '<style type="text/css" media="all">';
-  var close = '</style>';
+var MarkupAPI = MarkupAPI || {};
 
-  return open + styles + close;
-}
+(function($) {
+  MarkupAPI.insertStyles = function(styles) {
+    var open  = '<style type="text/css" media="all">';
+    var close = '</style>';
 
-function render(data) {
-  $('head').append(insertStyles(data.css));
-  $('#target').append(data.html);
-}
+    return open + styles + close;
+  };
+
+  MarkupAPI.render = function(data) {
+    $('head').append(
+      MarkupAPI.insertStyles(data.nav.css),
+      MarkupAPI.insertStyles(data.footer.css)
+    );
+
+    $('#markup-api-nav-target').append(data.nav.html);
+    $('#markup-api-footer-target').append(data.footer.html);
+  };
+}(jQuery));
